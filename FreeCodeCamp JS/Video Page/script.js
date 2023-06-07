f68e63103c5cc19be00e04a9fe37f968a93b49be
Nehal <mdnehaladil@gmail.com>
2023-06-07 16:36:45 +0530

    add play pause function

---
+++ FreeCodeCamp JS/Video Page/script.js
@@ -0,0 +1,16 @@
+console.log("Running");
+
+const btn = document.querySelector(".play-pause");
+const video = document.querySelector(".video-container");
+
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("slide")) {
+    // if the button does not include class 'slide' it will be added and video will be paused
+
+    btn.classList.add("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
