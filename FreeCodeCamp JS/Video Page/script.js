8776bf4f1f9fc6e89d540024f0037e4906b0882c
Nehal <mdnehaladil@gmail.com>
2023-06-07 16:58:39 +0530

    add apreloader

--- FreeCodeCamp JS/Video Page/script.js
+++ FreeCodeCamp JS/Video Page/script.js
@@ -14,3 +14,10 @@ btn.addEventListener("click", function () {
     video.play();
   }
 });
+
+//Preloader
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+});
