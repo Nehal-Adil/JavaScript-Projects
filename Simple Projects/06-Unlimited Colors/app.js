c20ebd368a6ebcf25fc3725862b5f27e6350a16e
Nehal <mdnehaladil@gmail.com>
2024-01-06 10:38:15 +0530

    unlimited colors

---
+++ Simple Projects/06-Unlimited Colors/app.js
@@ -0,0 +1,35 @@
+console.log("Unlimited Colors");
+
+// generate a random color
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+// console.log(Math.floor(Math.random() * 16));
+console.log(randomColor());
+
+let intervalId;
+
+const startChangingColor = function () {
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 500);
+  }
+};
+
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
